1bec182201935ec22691a2ae420b4b04cda1b323
Cristobal Fallas Mora <cristobalfallascr@hotmail.com>
2021-02-15 13:30:07 -0600

    Add context to Auth, connect Auth to backend signup

--- src/products/pages/Auth.js
+++ src/products/pages/Auth.js
@@ -1,9 +1,13 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { Link } from "react-router-dom";
 
 import { useForm } from "../../shared/hooks/form-hook";
+import { AuthContext } from "../../shared/context/auth-context";
 import Input from "../../shared/components/FormElements/Input";
 import Card from "../../shared/components/UIElements/Card";
+import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
+
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_REQUIRE,
@@ -15,6 +19,11 @@ import Button from "../../shared/components/FormElements/Button";
 import "./Auth.css";
 
 const Auth = () => {
+  const auth = useContext(AuthContext);
+  const [isLoginMode, setisLoginMode] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState();
+
   const [formState, inputHandler, setFormData] = useForm(
     {
       email: {
@@ -29,17 +38,72 @@ const Auth = () => {
     false
   );
 
-  //Form Submission handler
-  const submitHandler = (event) => {
-    event.preventDefault();
-    console.log(formState.inputs);
+  // This function handles the signup mode, either login mode or sigunp mode
+  const handleSignup = () => {
+    if (!isLoginMode) {
+      setFormData(
+        {
+          ...formState.inputs,
+          firstName: undefined,
+          lastName: undefined,
+          passwordConfirmation: undefined,
+        },
+        formState.inputs.email.isValid && formState.inputs.password.isValid
+      );
+    } else {
+      setFormData(
+        {
+          ...formState.inputs,
+          firstName: {
+            value: "",
+            isValid: false,
+          },
+          lastName: {
+            value: "",
+            isValid: false,
+          },
+        },
+        false
+      );
+    }
+
+    setisLoginMode((prev) => !prev);
+    
   };
 
-  const [signupMode, setSignupMode] = useState(false);
+  //Form Submission handler
+  const submitHandler = async (event) => {
+    event.preventDefault();
+  
 
-  const handleSignup = () => {
-    setSignupMode((prev) => !prev);
-    console.log(signupMode);
+    if (isLoginMode) {
+    } else {
+      try {
+        setIsLoading(true);
+        const response = await fetch("http://localhost:5000/api/users/signup", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            firstName: formState.inputs.firstName.value,
+            lastName: formState.inputs.lastName.value,
+            email: formState.inputs.email.value,
+            password: formState.inputs.password.value,
+            passwordConfirmation: formState.inputs.passwordConfirmation.value,
+            role: "default",
+          }),
+        });
+        const responseData = await response.json();
+        console.log(responseData);
+        setIsLoading(false);
+        auth.login();
+      } catch (err) {
+        console.log(err);
+        setIsLoading(false);
+        setError(err.message || "Algo malio Sal!");
+      }
+    }
   };
 
   return (
@@ -49,16 +113,28 @@ const Auth = () => {
           <h2>Accede a tu cuenta</h2>
         </div>
         <form autocomplete="off">
-          {signupMode && (
-            <Input
-              element="input"
-              type="text"
-              id="fullName"
-              validators={[VALIDATOR_REQUIRE()]}
-              errorText="Ingrese su nombre completo"
-              onInput={inputHandler}
-              label="Nombre y apellidos"
-            />
+          {!isLoginMode && (
+            <React.Fragment>
+              <Input
+                element="input"
+                type="text"
+                id="firstName"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Ingrese su nombre completo"
+                onInput={inputHandler}
+                label="Nombre"
+              />
+
+              <Input
+                element="input"
+                type="text"
+                id="lastName"
+                validators={[VALIDATOR_REQUIRE()]}
+                errorText="Ingrese sus apellidos"
+                onInput={inputHandler}
+                label="Apellidos"
+              />
+            </React.Fragment>
           )}
 
           <Input
@@ -81,7 +157,7 @@ const Auth = () => {
             label="Contrasena"
           />
 
-          {signupMode && (
+          {!isLoginMode && (
             <Input
               element="input"
               type="password"
@@ -93,7 +169,7 @@ const Auth = () => {
             />
           )}
 
-          {!signupMode && (
+          {isLoginMode && (
             <Button
               type="submit"
               onClick={submitHandler}
@@ -102,13 +178,13 @@ const Auth = () => {
               Acceder
             </Button>
           )}
-          {signupMode && (
+          {!isLoginMode && (
             <Button onClick={submitHandler} disabled={!formState.isValid}>
               Registrarme
             </Button>
           )}
         </form>
-        {signupMode === false ? (
+        {isLoginMode === true ? (
           <p>
             no tengo usuario? <Link onClick={handleSignup}>Registrarme</Link>
           </p>
